49ba23b3ef31378452dc97969a325b83c4ea22cd
kwojtyla <karolwojtyla360@gmail.com>
2024-11-20 15:02:45 -0300

    feat: add scroll to bottom button and follow chat logic

--- src/components/chat-content.tsx
+++ src/components/chat-content.tsx
@@ -3,12 +3,46 @@
 import { useChatContext } from '@/providers/chat-provider'
 import { ChatMessage } from '@/components/chat-message'
 import { ChatLoading } from '@/components/chat-loading'
+import { useEffect, useRef, useState } from 'react'
+import { Button } from './ui/button'
+import { ArrowDown } from 'lucide-react'
 
 export function ChatContent() {
   const { messages, isLoading } = useChatContext()
+  const [showGoToBottom, setShowGoToBottom] = useState(false)
+  const [isAtBottom, setIsAtBottom] = useState(true)
+  const chatEndRef = useRef<HTMLDivElement>(null)
+
+  const handleScroll = () => {
+    const scrollContainer = document.documentElement
+    const isNearBottom =
+      scrollContainer.scrollHeight -
+        scrollContainer.scrollTop -
+        window.innerHeight <
+      100
+    setShowGoToBottom(!isNearBottom)
+    setIsAtBottom(isNearBottom)
+  }
+
+  const scrollToBottom = () => {
+    chatEndRef.current?.scrollIntoView({ behavior: 'smooth' })
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll)
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [])
+
+  useEffect(() => {
+    if (isAtBottom) {
+      chatEndRef.current?.scrollIntoView({ behavior: 'smooth' })
+    }
+  }, [messages, isLoading, isAtBottom])
 
   return (
-    <main className="mx-auto h-full w-full grow md:max-w-3xl md:gap-5 lg:max-w-[40rem] lg:gap-6 xl:max-w-[48rem]">
+    <main className="relative mx-auto h-full w-full grow md:max-w-3xl md:gap-5 lg:max-w-[40rem] lg:gap-6 xl:max-w-[48rem]">
       <div className="mx-auto flex w-full flex-1 grow flex-col gap-4 p-6 text-base md:max-w-3xl md:gap-5 lg:max-w-[40rem] lg:gap-6 xl:max-w-[48rem]">
         <div className="flex-1 grow">
           <div className="mb-36 space-y-4 md:space-y-6">
@@ -19,9 +53,19 @@ export function ChatContent() {
             ) : (
               <ChatLoading />
             )}
+            <div ref={chatEndRef}></div>
           </div>
         </div>
       </div>
+      {showGoToBottom && (
+        <Button
+          size="icon"
+          onClick={scrollToBottom}
+          className="fixed bottom-28 left-1/2 -translate-x-1/2 transform rounded-full bg-primary p-3"
+        >
+          <ArrowDown />
+        </Button>
+      )}
     </main>
   )
 }
