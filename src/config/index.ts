2fc9262aadb5483a9cddf17707954a536731b9ff
Aridan Pantoja <aridanpantoja@gmail.com>
2024-11-25 11:55:57 -0300

    feat(config): add PS2025 link to site configurationo

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,4 +4,7 @@ export const siteConfig = {
   ogImage: 'https://chatbot-ufpa.vercel.app/og.png',
   description:
     'Chatbot não oficial para candidatos do PS 2025 UFPA, oferecendo informações sobre inscrições, cotas, documentação e muito mais.',
+  links: {
+    ps2025: 'https://www.ceps.ufpa.br/index.php/ps2025/848-ed-avisos-ps2025',
+  },
 }
